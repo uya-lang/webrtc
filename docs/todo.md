6d441fc242591575a95c5f2b3e96d940c1b0e4d3
winger <funnywwh@qq.com>
2026-05-31 18:33:54 +0800

    Implement TURN send and data indications

--- docs/todo.md
+++ docs/todo.md
@@ -176,7 +176,7 @@
 - [x] 实现 long-term credential。
 - [x] 实现 Refresh。
 - [x] 实现 CreatePermission。
-- [ ] 实现 Send/Data indication。
+- [x] 实现 Send/Data indication。
 - [ ] 实现 ChannelBind。
 - [ ] 实现 relay candidate 注入 ICE。
 - [ ] 实现 allocation refresh timer。
--- tests/check_phase6_turn.sh
+++ tests/check_phase6_turn.sh
@@ -20,6 +20,8 @@ rg -Fq "export struct TurnRefreshRequest" src/webrtc/turn/model.uya
 rg -Fq "export struct TurnRefreshSuccessResponse" src/webrtc/turn/model.uya
 rg -Fq "export struct TurnCreatePermissionRequest" src/webrtc/turn/model.uya
 rg -Fq "export struct TurnCreatePermissionSuccessResponse" src/webrtc/turn/model.uya
+rg -Fq "export struct TurnSendIndication" src/webrtc/turn/model.uya
+rg -Fq "export struct TurnDataIndication" src/webrtc/turn/model.uya
 rg -Fq "export struct TurnAuthenticationChallenge" src/webrtc/turn/model.uya
 rg -Fq "export struct TurnLongTermCredentials" src/webrtc/turn/model.uya
 
@@ -28,6 +30,8 @@ rg -Fq "export fn turn_long_term_credentials_apply_stale_nonce" src/webrtc/turn/
 rg -Fq "export fn turn_long_term_credentials_is_ready" src/webrtc/turn/auth.uya
 rg -Fq "export fn turn_parse_realm" src/webrtc/turn/parse.uya
 rg -Fq "export fn turn_parse_nonce" src/webrtc/turn/parse.uya
+rg -Fq "export fn turn_send_indication_parse" src/webrtc/turn/parse.uya
+rg -Fq "export fn turn_data_indication_parse" src/webrtc/turn/parse.uya
 rg -Fq "export fn turn_refresh_request_parse" src/webrtc/turn/parse.uya
 rg -Fq "export fn turn_refresh_success_response_parse" src/webrtc/turn/parse.uya
 rg -Fq "export fn turn_create_permission_request_parse" src/webrtc/turn/parse.uya
@@ -44,12 +48,15 @@ rg -Fq "export fn turn_builder_init_refresh_error_response" src/webrtc/turn/writ
 rg -Fq "export fn turn_builder_init_create_permission_request" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_init_create_permission_success_response" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_init_create_permission_error_response" src/webrtc/turn/write.uya
+rg -Fq "export fn turn_builder_init_send_indication" src/webrtc/turn/write.uya
+rg -Fq "export fn turn_builder_init_data_indication" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_init_allocate_request" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_init_allocate_success_response" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_append_requested_transport_udp" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_append_xor_relayed_address_ipv4" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_append_xor_peer_address_ipv4" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_append_xor_peer_address_ipv6" src/webrtc/turn/write.uya
+rg -Fq "export fn turn_builder_append_data" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_append_realm" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_append_nonce" src/webrtc/turn/write.uya
 rg -Fq "export fn turn_builder_append_long_term_credentials" src/webrtc/turn/write.uya
@@ -64,5 +71,7 @@ rg -Fq "turn_test_check_refresh_roundtrip" src/webrtc_turn_test_main.uya
 rg -Fq "turn_test_check_refresh_invalid_paths" src/webrtc_turn_test_main.uya
 rg -Fq "turn_test_check_create_permission_roundtrip" src/webrtc_turn_test_main.uya
 rg -Fq "turn_test_check_create_permission_invalid_paths" src/webrtc_turn_test_main.uya
+rg -Fq "turn_test_check_send_data_indication_roundtrip" src/webrtc_turn_test_main.uya
+rg -Fq "turn_test_check_send_data_indication_invalid_paths" src/webrtc_turn_test_main.uya
 
 ../uya/bin/uya run src/webrtc_turn_test_main.uya
